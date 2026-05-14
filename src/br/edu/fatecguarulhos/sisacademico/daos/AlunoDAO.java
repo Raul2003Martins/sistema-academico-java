c57d7963426efa7ddcf8296cfd474ec6e0e98c15
luciobrito <luciobp869@gmail.com>
2026-05-14 11:35:28 -0300

    AlunoDAO e novos testes

--- src/br/edu/fatecguarulhos/sisacademico/daos/AlunoDAO.java
+++ src/br/edu/fatecguarulhos/sisacademico/daos/AlunoDAO.java
@@ -12,27 +12,37 @@ import br.edu.fatecguarulhos.sisacademico.models.Aluno;
 import br.edu.fatecguarulhos.sisacademico.utils.ConnectionFactory;
 
 public class AlunoDAO {
+	public Aluno buscarAluno(Aluno aluno) throws Exception{
+		return buscarAluno(aluno.getRgm());
+	}
 	public Aluno buscarAluno(Integer RGM) throws Exception{
 		Aluno aluno = new Aluno();
 		String query = "SELECT "
-				+ "uf, nome, cpf, email, endereco, municipio, celular "
+				+ "nome, cpf, email, endereco, municipio, uf, celular "
 				+ "FROM aluno WHERE rgm = ?";
 		try{
 			Connection connection = ConnectionFactory.getConnection();
 			PreparedStatement pstmt = connection.prepareStatement(query);
 			pstmt.setInt(1, RGM);
 			ResultSet rs = pstmt.executeQuery();
-			if(rs.first()) {
+			if(rs.next()) {
 				aluno.setRgm(RGM);
 				aluno.setNome(rs.getString(1));
 				aluno.setCpf(rs.getString(2));
+				aluno.setEmail(rs.getString(3));
+				aluno.setEndereco(rs.getString(4));
+				aluno.setMunicipio(rs.getString(5));
+				aluno.setUf(rs.getString(6));
+				aluno.setCelular(rs.getString(7));
 			}
+			else aluno = null;
 			connection.close();
+
+			return aluno;
 		}
 		catch (SQLException e) {
-			throw new Exception("Erro no banco de dados");
+			throw e;
 		}
-		return aluno;
 	} 
 	
 	public int inserirAluno( Aluno aluno) throws Exception {
@@ -61,6 +71,35 @@ public class AlunoDAO {
 	
 	public void deletarAluno(Aluno aluno) {
 		deletarNotas();
+		String sql = "DELETE FROM aluno WHERE rgm = ?";
+		try {
+			Connection connection = ConnectionFactory.getConnection();
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, aluno.getRgm());
+			pstmt.execute();
+		}
+		catch(Exception e) {}
 	}
 	private void deletarNotas() {}
+	public void atualizarAluno(Aluno aluno) {
+		String sql = "UPDATE aluno SET "
+				+ "nome = ?, email = ?, cpf = ?, endereco = ?, municipio = ?, uf = ?, celular = ?"
+				+ "WHERE rgm = ?";
+		try {
+			Connection connection = ConnectionFactory.getConnection();
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, aluno.getNome());
+			pstmt.setString(2, aluno.getEmail());
+			pstmt.setString(3, aluno.getCpf());
+			pstmt.setString(4, aluno.getEndereco());
+			pstmt.setString(5, aluno.getMunicipio());
+			pstmt.setString(6, aluno.getUf());
+			pstmt.setString(7, aluno.getCelular());
+			pstmt.setInt(8, aluno.getRgm());
+			pstmt.execute();
+		}
+		catch(Exception e) {
+			
+		}
+	}
 }
--- src/br/edu/fatecguarulhos/sisacademico/tests/TesteAlunoDAO.java
+++ src/br/edu/fatecguarulhos/sisacademico/tests/TesteAlunoDAO.java
@@ -8,19 +8,28 @@ import br.edu.fatecguarulhos.sisacademico.daos.AlunoDAO;
 import br.edu.fatecguarulhos.sisacademico.models.Aluno;
 
 class TesteAlunoDAO {
-
+	private AlunoDAO dao;
+	TesteAlunoDAO(){
+		dao = new AlunoDAO();
+	}
 	@Test
 	void testBuscarAluno() {
-		fail("Not yet implemented");
+		assertDoesNotThrow(()->{
+			Aluno a = dao.buscarAluno(1);
+			System.out.println(a.getNome());
+			System.out.println(a.getCpf());
+		});
 	}
 
 	@Test
 	void testInserirAluno() {
-		AlunoDAO dao = new AlunoDAO();
 		assertDoesNotThrow(()->{
-			Aluno aluno = new Aluno(1,"Fulano","fulano@email","123", "Rua tal", "Cidadinha", "SP", "1190000");
+			if(dao.buscarAluno(2) == null) {
+				Aluno aluno = new Aluno(2,"Fulano","fulano@email","123", "Rua tal", "Cidadinha", "SP", "1190000");
+				assertTrue(dao.inserirAluno(aluno) == 1);
+				assertNotNull(dao.buscarAluno(2));
+			}
 			
-			assertTrue(dao.inserirAluno(aluno) == 1);
 		});
 		
 		
@@ -28,7 +37,26 @@ class TesteAlunoDAO {
 
 	@Test
 	void testDeletarAluno() {
-		fail("Not yet implemented");
+		assertDoesNotThrow(()->{
+			Aluno aluno = new Aluno(2,"Fulano","fulano@email","123", "Rua tal", "Cidadinha", "SP", "1190000");
+			//Se não existir um aluno com este rgm, criar
+			if(dao.buscarAluno(aluno.getRgm()) == null) dao.inserirAluno(aluno);
+			//deletar
+			dao.deletarAluno(aluno);
+			//Verificar se realmente foi deletado
+			assertNull(dao.buscarAluno(2));
+			//Inserir aluno de volta
+			dao.inserirAluno(aluno);
+		});
+	}
+	@Test
+	void testAtualizarAluno() {
+		assertDoesNotThrow(()->{
+			Aluno aluno = new Aluno(2,"atualizado","fulano@email","123", "Rua tal", "Cidadinha", "SP", "1190000");
+			dao.atualizarAluno(aluno);
+			assertTrue(dao.buscarAluno(aluno).getNome().equals(aluno.getNome()));
+			
+		});
 	}
 
 }
