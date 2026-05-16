304072b5a6a9778789a94b92ff0f58a8e8802c41
GGribeiro21 <160231622+GGribeiro21@users.noreply.github.com>
2026-05-16 20:29:58 -0300

    alteracoes na classe curso

--- src/br/edu/fatecguarulhos/sisacademico/models/Curso.java
+++ src/br/edu/fatecguarulhos/sisacademico/models/Curso.java
@@ -1,39 +1,70 @@
 package br.edu.fatecguarulhos.sisacademico.models;
 
 public class Curso {
-	public Curso(int codigo, String nome, String campus, String turno) {
+	
+
+
+	int codigo,rgm_aluno,codigo_curso;
+	String nome,campus,turno;
+
+	public Curso(int codigo, int rgm_aluno, int codigo_curso, String nome, String campus, String turno) {
 		super();
 		this.codigo = codigo;
+		this.rgm_aluno = rgm_aluno;
+		this.codigo_curso = codigo_curso;
 		this.nome = nome;
 		this.campus = campus;
 		this.turno = turno;
 	}
-	int codigo;
-	String nome,campus,turno;
+
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
+	public int getRgm_aluno() {
+		return rgm_aluno;
+	}
+
+	public void setRgm_aluno(int rgm_aluno) {
+		this.rgm_aluno = rgm_aluno;
+	}
+
+	public int getCodigo_curso() {
+		return codigo_curso;
+	}
+
+	public void setCodigo_curso(int codigo_curso) {
+		this.codigo_curso = codigo_curso;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCampus() {
 		return campus;
 	}
+
 	public void setCampus(String campus) {
 		this.campus = campus;
 	}
+
 	public String getTurno() {
 		return turno;
 	}
+
 	public void setTurno(String turno) {
 		this.turno = turno;
 	}
 	
+
 
 }
