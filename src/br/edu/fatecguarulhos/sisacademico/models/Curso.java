1e3caf72f54fab83ace15b4e5556c928dfc6f620
Abner Souza <abners4390@gmail.com>
2026-05-16 23:25:04 -0300

    Implementação do CursoDAO

--- src/br/edu/fatecguarulhos/sisacademico/models/Curso.java
+++ src/br/edu/fatecguarulhos/sisacademico/models/Curso.java
@@ -1,39 +1,54 @@
 package br.edu.fatecguarulhos.sisacademico.models;
 
 public class Curso {
+	
+	//Declaração de Atributos
+	private int codigo;
+	private String nome,campus,turno;
+	
+	//Inicialização do Construtor
 	public Curso(int codigo, String nome, String campus, String turno) {
-		super();
 		this.codigo = codigo;
 		this.nome = nome;
 		this.campus = campus;
-		this.turno = turno;
-	}
-	int codigo;
-	String nome,campus,turno;
-	public int getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCampus() {
-		return campus;
-	}
-	public void setCampus(String campus) {
-		this.campus = campus;
-	}
-	public String getTurno() {
-		return turno;
-	}
-	public void setTurno(String turno) {
 		this.turno = turno;
 	}
 	
+	//Construtor vazio na inicialização dos objetos
+	public Curso() {
+	}
+	
+	//Métodos Acessores e Modificadores (Getter e Setter)
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCampus() {
+		return campus;
+	}
+	
+	public void setCampus(String campus) {
+		this.campus = campus;
+	}
+	
+	public String getTurno() {
+		return turno;
+	}
+	
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
 
 }
--- src/br/edu/fatecguarulhos/sisacademico/models/Disciplina.java
+++ src/br/edu/fatecguarulhos/sisacademico/models/Disciplina.java
@@ -2,15 +2,15 @@ package br.edu.fatecguarulhos.sisacademico.models;
 
 public class Disciplina {
 	
+	//Declaração de Atributos
 	private Aluno aluno;
 	private Curso curso;
 	private int codigo,faltas;
 	private String nome,semestre;
 	private float nota;
 	
-	public Disciplina(int codigo, int rgm_aluno, int codigo_curso, int faltas, String nome, String semestre,
-			float nota) {
-		super();
+	//Inicialização do Construtor
+	public Disciplina(int codigo, int rgm_aluno, int codigo_curso, int faltas, String nome, String semestre, float nota) {
 		this.codigo = codigo;
 		this.faltas = faltas;
 		this.nome = nome;
@@ -18,34 +18,49 @@ public class Disciplina {
 		this.nota = nota;
 	}
 	
+	//Construtor vazio para evitar erros na inicialização dos objetos
+	public Disciplina() {
+	}
+	
+	//Métodos Acessores e Modifcadores (Getters e Setters)
 	public int getCodigo() {
 		return codigo;
 	}
+	
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+	
 	public int getFaltas() {
 		return faltas;
 	}
+	
 	public void setFaltas(int faltas) {
 		this.faltas = faltas;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getSemestre() {
 		return semestre;
 	}
+	
 	public void setSemestre(String semestre) {
 		this.semestre = semestre;
 	}
+	
 	public float getNota() {
 		return nota;
 	}
+	
 	public void setNota(float nota) {
 		this.nota = nota;
 	}
+	
 }
